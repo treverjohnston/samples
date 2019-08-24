6da3f34bdeab1838089371a102a415572ca7083e
Trever Johnston <treverj1029@gmail.com>
2019-08-24 00:03:31 -0600

    Fixed pics i think

--- src/store/samples/state.js
+++ src/store/samples/state.js
@@ -4,21 +4,21 @@ export default {
     {
       title: 'Lawn Care',
       description: 'Sample lawn care site',
-      img: '../assets/lawn.png',
+      img: './statics/pics/lawn.png',
       link: 'https://e-sample-site.herokuapp.com/',
       id: 0
     },
     {
       title: 'Ride For Hope Idaho',
       description: 'Charity bike ride website',
-      img: '../assets/RFHIdahoLogo.png',
+      img: './statics/pics/RFHIdahoLogo.png',
       link: 'https://rideforhopeidaho.com/',
       id: 2
     },
     {
       title: 'Ebenezer Websites',
       description: 'Homepage for Ebenezer Websites LLC',
-      img: '../assets/white-ee.png',
+      img: './statics/pics/white-ee.png',
       link: 'https://ebenezerwebsites.com/',
       id: 1
     },
